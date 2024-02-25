6120991f49990be4cd149f20e50848bc6abc94ae
marioskaraiskossbusiness <marioskaraiskosbusiness@gmail.com>
2024-02-25 18:34:19 +0200

    first commit

---
+++ src/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    String name;
+    int accID;
+    int balance;
+
+    public Account(String name, int accID, int balance) {
+        this.name = name;
+        this.accID = accID;
+        this.balance = balance;
+    }
+
+    public void results() {
+        System.out.println("The name is " + name + ", the account ID is " + accID +
+                ", and the balance of this account is " + balance + " euros");
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+        System.out.println("The amount you deposited was " + amount +
+                " and the new balance after your deposit is: " + balance);
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Account person1 = new Account("marios", 01, 4000);
+        Account person2 = new Account("john", 02, 20);
+        Account person3 = new Account("nikos", 03, 10000);
+        Account person4 = new Account("eirini", 04, 5);
+
+        //first scanner for id
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your ID: ");
+        int personID = scanner.nextInt();
+
+        //Second Scanner for amount that you deposit
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("enter the amount of money you want to deposit: ");
+        int deposited = scanner1.nextInt();
+
+    }
+}
